67bb631caa43857cde40f3495a508c90b3bed51e
Iskander <adiullov.iskander2000@gmail.com>
2022-03-26 11:07:31 +0300

    Add conect to db

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
-	"log"
+	"os"
 
 	"github.com/IskanderA1/todo-server"
 	"github.com/IskanderA1/todo-server/pkg/handler"
 	"github.com/IskanderA1/todo-server/pkg/repository"
 	"github.com/IskanderA1/todo-server/pkg/service"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
-	repository := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error configs: %s", err.Error())
+	}
+
+	if err := gotenv.Load(); err != nil {
+		logrus.Fatalf("error env: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMod:   viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		logrus.Fatalf("error init db: %s", err.Error())
+	}
+
+	repository := repository.NewRepository(db)
 	service := service.NewService(repository)
 	handlers := handler.NewHandler(service)
 
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error http sever: %s", err.Error())
+		logrus.Fatalf("error http sever: %s", err.Error())
 	}
 }
 
---
+++ pkg/repository/postgres.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMod   string
+}
+
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMod)
+	db, err := sqlx.Open("postgres", dataSourceName)
+	println(dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
