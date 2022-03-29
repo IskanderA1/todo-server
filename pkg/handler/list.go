30051805d7248dda55c88b176cb8abebd9bd772b
Iskander <adiullov.iskander2000@gmail.com>
2022-03-29 19:52:00 +0300

    Add todo-list post, get endpoint

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -2,24 +2,72 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/IskanderA1/todo-server"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-	id, _ := c.Get(userCtx)
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.service.TodoList.Create(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
 }
 
-func (h *Handler) getAllList(c *gin.Context) {
+type getAllListsResponse struct {
+	Data []todo.TodoList `json:"data"`
+}
 
+func (h *Handler) getAllList(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	lists, err := h.service.TodoList.GetAll(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, getAllListsResponse{
+		Data: lists,
+	})
 }
 
 func (h *Handler) getListById(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, "invalid param")
+		return
+	}
+
+	todoList, err := h.service.TodoList.GetById(userId, id)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, todoList)
 }
 
 func (h *Handler) updateList(c *gin.Context) {
---
+++ pkg/service/todo_list.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"github.com/IskanderA1/todo-server"
+	"github.com/IskanderA1/todo-server/pkg/repository"
+)
+
+type TodoListService struct {
+	repo repository.TodoList
+}
+
+func NewTodoListService(repo repository.TodoList) *TodoListService {
+	return &TodoListService{repo: repo}
+}
+
+func (s *TodoListService) Create(userId int, list todo.TodoList) (int, error) {
+	return s.repo.Create(userId, list)
+}
+func (s *TodoListService) GetAll(userId int) ([]todo.TodoList, error) {
+	return s.repo.GetAll(userId)
+}
+
+func (s *TodoListService) GetById(userId, id int) (todo.TodoList, error) {
+	return s.repo.GetById(userId, id)
+}
